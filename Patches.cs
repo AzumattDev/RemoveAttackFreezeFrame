769bd1f0b11a6c202912525b106363d7bed15e1c
AzumattDev <azumattgaming@gmail.com>
2023-01-12 12:21:30 -0500

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace RemoveAttackFreezeFrame;
+
+[HarmonyPatch(typeof(Character),nameof(Character.FreezeFrame))]
+static class CharacterFreezeFramePatch
+{
+    static void Prefix(Character __instance, ref float duration)
+    {
+        if (__instance is not Player player) return;
+        if (player == Player.m_localPlayer)
+        {
+            duration = 0.0f;
+        }
+    }
+    static void Postfix(Character __instance, ref float duration)
+    {
+        if (__instance is not Player player) return;
+        if (player == Player.m_localPlayer)
+        {
+            duration = 0.0f; // This is just here to make sure the duration is 0.0f. It's not needed...technically.
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace RemoveAttackFreezeFrame
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class RemoveAttackFreezeFramePlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "RemoveAttackFreezeFrame";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource RemoveAttackFreezeFrameLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+        }
+    }
+}
